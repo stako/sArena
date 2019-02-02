e26239ec88fc5b9b312c967baca02085d7e63e15
Stako <stako@users.noreply.github.com>
2019-02-01 19:54:04 -0800

    remove howl of terror

--- DRList.lua
+++ DRList.lua
@@ -13,7 +13,6 @@ sArenaMixin.drList = {
     [226943]  = "Disorient",       -- Mind Bomb
     [2094]    = "Disorient",       -- Blind
     [118699]  = "Disorient",       -- Fear
-    [5484]    = "Disorient",       -- Howl of Terror
     [6358]    = "Disorient",       -- Seduction (Succubus)
     [115268]  = "Disorient",       -- Mesmerize (Shivarra)
     [5246]    = "Disorient",       -- Intimidating Shout
