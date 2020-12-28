5f22f01b0a1210b3567e7bd85cd956025d5265e1
Stako <stako@users.noreply.github.com>
2020-12-28 15:31:39 -0800

    improve trinket logic
    trinket will go on CD when human & undead racials are used

--- Modules/Racials.lua
+++ Modules/Racials.lua
@@ -1,3 +1,5 @@
+local GetTime = GetTime
+
 sArenaMixin.defaultSettings.profile.racialCategories = {
     ["Human"] = true,
     ["Scourge"] = false,
@@ -13,20 +15,41 @@ local racialData = {
     ["Scourge"] = { texture = GetSpellTexture(7744), sharedCD = 30 },
 }
 
+local function GetRemainingCD(frame)
+    local startTime, duration = frame:GetCooldownTimes()
+    if ( startTime == 0 ) then return 0 end
+
+    local currTime = GetTime()
+
+    return startTime + duration - currTime
+end
+
 function sArenaFrameMixin:FindRacial(event, spellID)
     if ( event ~= "SPELL_CAST_SUCCESS" ) then return end
 
     local duration = racialSpells[spellID]
 
-    if ( duration and self.Racial.Texture:GetTexture() ) then
-        self.Racial.Cooldown:SetCooldown(GetTime(), duration)
-    elseif ( (spellID == 336126 or spellID == 336135) and self.Racial.Texture:GetTexture() ) then
-        local activeStartTime, activeDuration = self.Racial.Cooldown:GetCooldownTimes()
+    if ( duration ) then
         local currTime = GetTime()
+
+        if ( self.Racial.Texture:GetTexture() ) then
+            self.Racial.Cooldown:SetCooldown(currTime, duration)
+        end
+
+        if ( self.Trinket.spellID == 336126 or self.Trinket.spellID == 336135 ) then
+            local remainingCD = GetRemainingCD(self.Trinket.Cooldown)
+            local sharedCD = racialData[self.race].sharedCD
+
+            if ( sharedCD and remainingCD < sharedCD ) then
+                self.Trinket.Cooldown:SetCooldown(currTime, sharedCD)
+            end
+        end
+    elseif ( (spellID == 336126 or spellID == 336135) and self.Racial.Texture:GetTexture() ) then
+        local remainingCD = GetRemainingCD(self.Racial.Cooldown)
         local sharedCD = racialData[self.race].sharedCD
 
-        if ( sharedCD and (activeStartTime + activeDuration - currTime) <= racialData[self.race].sharedCD ) then
-            self.Racial.Cooldown:SetCooldown(currTime, sharedCD)
+        if ( sharedCD and remainingCD < sharedCD ) then
+            self.Racial.Cooldown:SetCooldown(GetTime(), sharedCD)
         end
     end
 end
